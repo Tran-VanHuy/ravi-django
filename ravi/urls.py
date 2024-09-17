178a9037b4954601176a626e6017e1d9f544afa6
Tran-VanHuy <huy1821051049@gmail.com>
2024-09-17 13:35:16 +0700

    update

--- ravi/urls.py
+++ ravi/urls.py
@@ -33,7 +33,8 @@ urlpatterns = [
     path('du-an', ProjectListPage.as_view(), name="project-list"),
     path('ve-chung-toi/<id>', AboutPage.as_view(), name="about"),
     path('tuyen-dung', JobListPage.as_view(), name="job-list"),
-    path('tuyen-dung/<id>', JobOpening.as_view(), name="job-opening")
+    path('tuyen-dung/<id>', JobOpening.as_view(), name="job-opening"),
+    path('linh-vuc/<id>', ActionDetailPage.as_view(), name="action-detail"),
 ]
 
 urlpatterns +=static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
--- ui/views.py
+++ ui/views.py
@@ -33,3 +33,6 @@ class JobOpening(TemplateView):
 def error_404(request, exception):
     print("vào đay 2")
     return render(request, '404.html', status=404)
+
+class ActionDetailPage(TemplateView):
+    template_name = "action-detail/index.html"
